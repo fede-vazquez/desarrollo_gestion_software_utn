54d497357ad28eb8842f214f94691e4be28fa83c
Vazquez <fvazquez@FRSN.UTN.EDU.AR>
2025-08-04 17:48:51 -0300

    Agrega clase sobre casos de prueba.

---
+++ prueba.js
@@ -0,0 +1,12 @@
+function pruebaMayor(a, b, c){
+    if (a>b && a>c){
+        console.log(a)
+    }
+    else{
+        if( b>a && b>c){
+            console.log(b)
+        }else{
+            console.log(c);
+        }
+    }
+}
